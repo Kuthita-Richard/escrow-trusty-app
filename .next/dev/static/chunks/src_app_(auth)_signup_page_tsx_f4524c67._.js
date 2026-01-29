0d32459a45bcf156ae08ef85ec858ad225a4ac16
Kuthita-Richard <richardkuthitawork@gmail.com>
2026-01-28 22:33:59 -0800

    Ignore Next.js build output and normalize line endings

--- .next/dev/static/chunks/src_app_(auth)_signup_page_tsx_f4524c67._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_3190e2eb._.js",
-  "static/chunks/src_cf1f1e33._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/src_app_not-found_tsx_f4524c67._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_96ff39ea._.js",
-  "static/chunks/node_modules_dca379be._.js"
-],
-    source: "dynamic"
-});
